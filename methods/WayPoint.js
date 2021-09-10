1c28ae9bc998f31285250ec5c5a6592b4f1120e9
root <root@493137-ch65794.tmweb.ru>
2021-09-11 01:00:51 +0300

    refactoring

--- methods/WayPoint.js
+++ methods/WayPoint.js
@@ -24,18 +24,11 @@ const WayPoint = async (params, client) => {
   try {
     const { NickName } = await client.query('GetDetailedPlayerInfo', [params.login]);
 
-    const ms = fns.format(params.racetime, 'SSS');
-    const secs = fns.format(params.racetime, 'ss');
-    const mins = fns.format(params.racetime, 'mm');
+    const timeString = fns.format(params.racetime, 'mm:ss:SSS');
 
-    const timeString = `${mins}:${secs}:${ms}`
+    if (!params.isendlap) return;
 
-    if (params.isendlap) {
-      server.log(`$o$0f0${NickName}$g finishes in $o$0f0${timeString}`);
-      return;
-    }
-
-    server.log(`$o$0f0${NickName}$g pass $o$0f0${params.checkpointinrace}$g checkpoint in $o$0f0${timeString}`);
+    server.log(`$o$0f0${NickName}$g finishes in $o$0f0${timeString}`);
   } catch (err) {
     log.red('Something went wrong in PlayerConnect');
     log.red(err);
