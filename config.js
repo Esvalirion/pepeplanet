2b9637cb5d700ecb8ee94f125918c4b9d959260c
Максим Новиков <mnovikov@sports.ru>
2021-09-10 03:42:33 +0300

    add connection, send message to chat

--- config.js
+++ config.js
@@ -8,39 +8,21 @@ const config = {
     login: 'SuperAdmin',
     password: 'SuperAdmin',
 
-    // Game: TM2020
-    game: 'TM2020',
-
-    // The login of the server at the master server, look it up on the player page in doubt
-    server_login: 'blah',
-
-    // The matchsettings file the server is started with
-    matchsettings_file: 'set.txt',
+    // The login of the server at the master server, look it up on the https://players.trackmania.com/
+    server_login: 'login',
   },
-
-  usedDatabase: 'mysql',
 
   // MySQL database settings
   mySql: {
-    // host and port
     host: 'localhost',
     port: 3306,
-
-    // user and password
     user: 'root',
     password: 'password',
-
-    // database name
-    database: 'nodecontrol',
+    dbname: 'nodecontrol',
   },
 
-  // List of disabled plugins by their name
-  // disabledPlugins: ['Sample Plugin'],
-
-  // List of administrators by their logins
-  admins: [
-    'your login here',
-  ],
+  // Administrators logins from https://trackmania.io/#/player
+  admins: ['login'],
 
 };
 
--- nodeplanet.js
+++ nodeplanet.js
@@ -9,10 +9,18 @@ const nodeplanet = {
 
     client.on('error', () => {
       log.red('Could not connect to server');
+      process.exit();
     });
 
-    client.on('connect', () => {
+    client.on('connect', async () => {
       log.green('Connected!');
+
+      await client.query('Authenticate', [config.trackmania.login, config.trackmania.password]).catch(() => {
+        log.red('Authentication failed');
+        process.exit();
+      });
+
+      client.query('ChatSendServerMessage', ['How it feels, pepegas, nodeplanet is here!']);
     });
   },
 };
