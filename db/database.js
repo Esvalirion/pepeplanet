d2d7b7664ba6b696372eab74cae9d85d982c0bf1
il12 <il5498@yandex.ru>
2021-09-13 15:54:14 +0300

    Add function to deploy db structure from js. Fixed sql script. Now you are free to change name of your database in config.

--- db/database.js
+++ db/database.js
@@ -1,7 +1,50 @@
+import fs from 'fs';
+import pool from './pool.js';
+import log from '../utils/log.js';
 import players from './players.js';
 import records from './records.js';
+
+/**
+ *  Execute sql script which checks if all necessary tables exist in the MySQL database and creates
+ *  them if necessary
+ * */
+const mysqlCheckAndDeployTables = async () => {
+  // get sql from file
+  const sqlToCreateMissingTables = await fs.promises.readFile('databases/nextcontrol.sql', 'utf-8')
+    .catch((e) => {
+      log.red(`FileSystem Error: ${JSON.stringify(e, null, 2)}`);
+      process.exit(1);
+    });
+
+  // remove all unnecessary symbols from sql
+  const formattedSql = sqlToCreateMissingTables.replace(/\r\n/g, ' ');
+
+  // split sql to single queries
+  const queries = formattedSql.split(';');
+
+  // promise to use as initial value in reduce
+  const starterPromise = Promise.resolve(null);
+
+  log.green('Database deploying started');
+  // create tables if missing
+  await queries.reduce(async (promise, query) => {
+    // evade case with whitespace query
+    if (query.length > 1) {
+      // create table if missing
+      promise.then(
+        () => pool.query(query).then(() => log.white(`Query ${query} executed correctly`))
+          .catch((e) => {
+            log.red(`MySQL database error: ${JSON.stringify(e, null, 2)}`);
+            process.exit(1);
+          }),
+      );
+    }
+  },
+  starterPromise);
+};
 
 export default {
   players,
   records,
+  mysqlCheckAndDeployTables,
 };
