cc6613543d1ed23b3c63be6e5dc1e6e6a68f9211
Max Novikov <esvalirion@gmail.com>
2021-09-14 18:15:51 +0300

    oops fix

--- config.js
+++ config.js
@@ -1,17 +1,15 @@
 const config = {
   // Trackmania server settings
   trackmania: {
-    // host, if you wanna run controller on local machine
-    host: 'localhost',
-    // server port on localhost, prefer 5004
-    port: 5004,
+    // server port on localhost, don't use 5000 please
+    port: 5555,
 
     // Authentication details for SuperAdmin
     login: 'SuperAdmin',
     password: 'SuperAdmin',
 
     // The login of the server at the master server, look it up on the https://players.trackmania.com/
-    serverLogin: 'tmspirit1',
+    serverLogin: 'login',
   },
 
   // MySQL database settings
@@ -19,18 +17,15 @@ const config = {
     host: 'localhost',
     port: 3306,
     user: 'pepega',
-    password: 'admiralpepega',
-    database: 'pepeplanet',
+    password: 'password',
+    database: 'pepedb',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
   },
 
   // Administrators logins from https://trackmania.io/#/player
-  admins: [
-    '-DYRTX1vQmeKs9Vo0gxeGw', // kaolin_sama_TTV
-    'df_cg2K_R_mqj2b8hcWngQ' // esvalirion
-  ],
+  admins: ['login'],
 };
 
 export default config;
