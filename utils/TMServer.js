a093e3b5b6e00a9f753471051496babce1979de9
il12 <il5498@yandex.ru>
2021-09-18 17:12:49 +0300

    Add: TMServer start/stop/restart functions for linux; Fix: Stop function now uses internal call and stopping only one TM Server.

--- utils/TMServer.js
+++ utils/TMServer.js
@@ -1,4 +1,4 @@
-import { exec, spawn } from 'child_process';
+import { spawn } from 'child_process';
 import process from 'process';
 import path from 'path';
 import log from './log.js';
@@ -13,26 +13,9 @@ const stringifyStartOptions = (options) => Object.keys(options).reduce((acc, key
   return acc;
 }, '');
 
-const stopTMDedicatedServer = () => {
+const stopTMDedicatedServer = async (client) => {
   try {
-    const ENV_PLATFORM = process.platform;
-    if (ENV_PLATFORM === 'win32') {
-      exec(' chcp 65001; Get-Process TrackmaniaServer -ErrorAction SilentlyContinue | Stop-Process -PassThru;',
-        {
-          shell: 'powershell.exe',
-          encoding: 'utf8',
-          cwd: config.TMDS.pathToTMServerDirectory,
-        }, (e, stdout, stderr) => {
-          if (e) {
-            log.red('Couldn\'t stop TM Server, admiral');
-            log.red(JSON.stringify(e, null, 2));
-          }
-        });
-    } else if (ENV_PLATFORM === 'linux') {
-      // TODO:
-    } else {
-      log.red('This function has not yet been implemented on your architecture');
-    }
+    await client.query('StopServer');
   } catch (e) {
     log.red('Something wrong in stopTMDedicatedServer, admiral');
     log.red(JSON.stringify(e, null, 2));
@@ -56,7 +39,13 @@ const startTMDedicatedServer = () => {
         cwd: config.TMDS.pathToTMServerDirectory,
       });
     } else if (ENV_PLATFORM === 'linux') {
-      // TODO:
+      const TMServerFile = path.join(config.TMDS.pathToTMServerDirectory, 'TrackmaniaServer');
+
+      const TMServer = spawn(`${TMServerFile} ${stringifyStartOptions(TMServerOptions)}`,
+        {
+          encoding: 'utf8',
+          cwd: config.TMDS.pathToTMServerDirectory,
+        });
     } else {
       log.red('This function has not yet been implemented on your architecture');
     }
@@ -67,33 +56,14 @@ const startTMDedicatedServer = () => {
   }
 };
 
-const restartTMDedicatedServer = () => {
-  try {
-    const TMServerOptions = config.TMDS.options;
-
-    const ENV_PLATFORM = process.platform;
-    if (ENV_PLATFORM === 'win32') {
-      const TMServerFile = path.join(config.TMDS.pathToTMServerDirectory, 'TrackmaniaServer.exe');
-
-      const TMServer = spawn(`
-        chcp 65001;
-        Get-Process TrackmaniaServer -ErrorAction SilentlyContinue | Stop-Process -PassThru;
-        ${TMServerFile} ${stringifyStartOptions(TMServerOptions)}`,
-      {
-        shell: 'powershell.exe',
-        encoding: 'utf8',
-        cwd: config.TMDS.pathToTMServerDirectory,
-      });
-    } else if (ENV_PLATFORM === 'linux') {
-      // TODO:
-    } else {
-      log.red('This function has not yet been implemented on your architecture');
-    }
-  } catch (e) {
+const restartTMDedicatedServer = (client) => {
+  client.query('StopServer').then(() => {
+    startTMDedicatedServer();
+  }).catch((e) => {
     log.red('Something wrong in restartTMDedicatedServer, admiral');
     log.red(JSON.stringify(e, null, 2));
     process.exit(1);
-  }
+  });
 };
 
 export default {
